b9e8997ae1b0e556bbd7d14c5ed45d86614e3d18
pannaian <pannaian@corp.netease.com>
2020-07-17 16:32:04 +0800

    增加直播、点播视频截图查询nodeJS demo

--- videoImageQueryDemo.js
+++ videoImageQueryDemo.js
@@ -5,24 +5,25 @@ var secretId="your_secret_id";
 var secretKey="your_secret_key";
 // 业务ID，易盾根据产品业务特点分配 
 var businessId="your_business_id";
-// 易盾反垃圾云服务直播查询检测结果接口地址
-var apiurl="http://as.dun.163yun.com/v1/livevideo/query/image";
+// 易盾反垃圾云服务直播离线结果获取接口地址 
+var apiurl="http://as.dun.163.com/v1/video/query/image";
 
 //请求参数
 var post_data = {
 	// 1.设置公有有参数
 	secretId:secretId,
 	businessId:businessId,
-	version:"v1.1",
+	version:"v1",
 	timestamp:new Date().getTime(),
-	nonce:utils.noncer()
+	nonce:utils.noncer(),
+	// 2.设置私有参数
+	taskId:"87aa24884d614ae8b8cc4d472b37be51",
+	levels:"[0,1,2]",
+	pageNum:"1",
+	pageSize:"20",
+	callbackStatus:"1",	// 详情查看官网CallbackStatus
+	orderType:"3"	    // 详情查看官网LiveVideoOrderType
 };
-// 设置私有参数
-post_data.taskId = "00bf1fc389da4413818172e43773cefd";
-post_data.levels = "[1,2]";
-post_data.callbackStatus = "1";
-post_data.pageNum = "1";
-post_data.pageSize = "10";
 var signature=utils.genSignature(secretKey,post_data);
 post_data.signature=signature;
 //http请求结果
@@ -30,29 +31,30 @@ var responseCallback=function(responseData){
 	var data = JSON.parse(responseData);
 	var code=data.code;
 	var msg=data.msg;
-	if (code == 200) {
-		var result=data.result;
+	if(code==200){
+		var result = data.result;
 		var status = result.status;
-		var images = result.images;
-		var count = images.count;
-		var rows = images.rows;
 		if (status == 0) {
-			rows.forEach(row => {
+			var images = result.images;
+			var count = images.count;
+			var rows = images.rows;
+			for (var i=0;i<rows.length;i++) {
+				var row = rows[i];
 				var url = row.url;
 				var label = row.label;
-				var labelLevel = row.labelLevel;
+				var labelLevel = row.labelLabel;
 				var callbackStatus = row.callbackStatus;
 				var beginTime = row.beginTime;
-				var endTime = row.endTime;
-			});
-			console.log("live data query success, images: " + JSON.stringify(rows));
+				var endTime = row.endTIme;
+				console.log("成功, count="+count+",url="+url+",label="+label+",labelLevel="+labelLevel+",callbackStatus="+callbackStatus+",开始时间="+beginTime+",结束时间="+endTime)
+			}
 		} else if (status == 20) {
-			console.log("taskIs is expired");
+			console.log("taskId不是7天内数据");
 		} else if (status == 30) {
-			console.log("taskId is not exist");
+			console.log("taskId不存在");
 		}
-	} else{
+	}else{
 		console.log('ERROR:code=' + code+',msg='+msg);
 	}
-};
+}
 utils.sendHttpRequest(apiurl,"POST",post_data,responseCallback);
