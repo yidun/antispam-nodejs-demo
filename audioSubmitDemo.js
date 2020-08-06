108ccecc6257dce70c80d1abfa3265bd54b3f4d3
maxiaofeng <maxiaofeng@corp.netease.com>
2020-08-06 19:52:42 +0800

    语音类业务升级二级细分类结构

--- audioSubmitDemo.js
+++ audioSubmitDemo.js
@@ -12,7 +12,8 @@ var post_data = {
     // 1.设置公有有参数
     secretId:secretId,
     businessId:businessId,
-    version:"v3.1",
+    // 点播语音版本v3.2及以上二级细分类结构进行调整
+    version:"v3.2",
     timestamp:new Date().getTime(),
     nonce:utils.noncer(),
     // 2.设置私有参数
--- videoSolutionCallbackDemo.js
+++ videoSolutionCallbackDemo.js
@@ -10,7 +10,8 @@ var apiurl="http://as.dun.163.com/v1/videosolution/callback/results";
 var post_data = {
 	// 1.设置公有有参数
 	secretId:secretId,
-	version:"v1",
+	// 点播音视频解决方案版本v1.1及以上语音二级细分类subLabels结构进行调整
+	version:"v1.1",
 	timestamp:new Date().getTime(),
 	nonce:utils.noncer()
 };
@@ -32,6 +33,36 @@ var responseCallback=function(responseData){
                 var taskId = obj.taskId;
                 var result = obj.result;
                 console.log("taskId="+taskId+",result="+result);
+                var evidences = obj.evidences;
+                if(evidences){
+                    var audio = evidences.audio;
+                    if(audio){
+                        var asrStatus=audio.asrStatus;
+                        if(asrStatus==4){
+                            console.log("检测失败taskId="+taskId+",asrStatus="+asrStatus);
+                        }else{
+                            var action=audio.action;
+                            if(action == 0){
+                                console.log("结果：通过，taskId="+taskId);
+                            }else if(action == 1 || action == 2){
+                                var labels=audio.labels;
+                                for(var k=0;k<labels.length;k++){
+                                     var labelInfo=labels[k];
+                                     var label = labelInfo.label;
+                                     var level = labelInfo.level;
+                                     // 注意二级细分类结构
+                                     var subLabels = labelInfo.subLabels;
+                                     for(var k=0;k<subLabels.length;k++) {
+                                         var subLabelObj = subLabels[k];
+                                         var subLabel = subLabelObj.subLabel;
+                                         var details = subLabelObj.details;
+                                         var hintArr = details.hint;
+                                     }
+                                }
+                            }
+                        }
+                    }
+                }
 			}
 		}
 	}else{
